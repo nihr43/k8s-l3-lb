0caf7e190d1775fb635bd50334778e3631d564e8
nathan hensel <nathanihensel@gmail.com>
2024-02-01 23:18:26 -0600

    no import abstraction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-'''
+"""
 >>> import netifaces
 
 No common machine should have address 1.1.1.1:
@@ -25,23 +25,29 @@ True
 {'family': 'inet', 'address': '127.0.0.1', 'netmask': '8', 'scope': 'local'}
 >>> inst.stop(wait=True)
 >>> inst.delete(wait=True)
-'''
+"""
 
+import os
+import socket
+import netifaces
+import logging
+import ipaddress
 import random
 from time import sleep
+from kubernetes import client, config
 
 
 def get_address_state(dev: str, address: str, netifaces) -> bool:
-    '''
+    """
     determine whether a given address is already assigned
-    '''
+    """
     parsed_addresses = []
     ifaces = netifaces.ifaddresses(dev)
 
     for i in ifaces:
         for j in ifaces[i]:
             for a, b in j.items():
-                if a == 'addr':
+                if a == "addr":
                     parsed_addresses.append(b)
 
     if address in parsed_addresses:
@@ -50,29 +56,33 @@ def get_address_state(dev: str, address: str, netifaces) -> bool:
         return False
 
 
-def provision_address(dev: str, address: str, netmask: str, logging, netifaces, os) -> None:
-    '''
+def provision_address(
+    dev: str, address: str, netmask: str, logging, netifaces, os
+) -> None:
+    """
     assure an address is assigned to a device
-    '''
+    """
     if get_address_state(dev, address, netifaces) is False:
-        logging.info('assuming address ' + address)
-        os.system('ip address add ' + address + netmask + ' dev ' + dev)
+        logging.info("assuming address " + address)
+        os.system("ip address add " + address + netmask + " dev " + dev)
 
 
-def enforce_no_address(dev: str, address: str, netmask: str, logging, netifaces, os) -> None:
-    '''
+def enforce_no_address(
+    dev: str, address: str, netmask: str, logging, netifaces, os
+) -> None:
+    """
     assure an address is not assigned to a device
-    '''
+    """
     if get_address_state(dev, address, netifaces) is True:
-        logging.info('forfeiting address ' + address)
-        os.system('ip address del ' + address + netmask + ' dev ' + dev)
+        logging.info("forfeiting address " + address)
+        os.system("ip address del " + address + netmask + " dev " + dev)
 
 
 def local_pod_match(pods, lb, current_node, logging) -> bool:
-    '''
+    """
     determine if a LoadBalancer's 'selector' matches any local pods.
     this is a determining factor whether we enforce the address.
-    '''
+    """
     matched_pods = []
     for pod in pods.items:
         if pod.spec.node_name == current_node:
@@ -89,40 +99,42 @@ def local_pod_match(pods, lb, current_node, logging) -> bool:
 
 
 def get_pods(client):
-    '''
+    """
     get all pods
-    '''
+    """
     api = client.CoreV1Api()
     pods = api.list_pod_for_all_namespaces()
     return pods
 
 
 def get_loadbalancers(client):
-    '''
+    """
     get a list of services of type LoadBalancer
-    '''
+    """
     api = client.CoreV1Api()
     lbs = []
     for service in api.list_service_for_all_namespaces().items:
-        if service.spec.type == 'LoadBalancer':
+        if service.spec.type == "LoadBalancer":
             lbs.append(service)
     return lbs
 
 
 def existing_ips_in_range(dev, netifaces, net_range, ipaddress):
-    '''
+    """
     get a list of all ips in range which are currently assigned to an interface
-    '''
+    """
     parsed_addresses = []
     ifaces = netifaces.ifaddresses(dev)
 
     for i in ifaces:
         for j in ifaces[i]:
             for a, b in j.items():
-                if a == 'addr':
+                if a == "addr":
                     try:
                         if type(ipaddress.ip_address(b)) is ipaddress.IPv4Address:
-                            if ipaddress.IPv4Address(b) in ipaddress.IPv4Network(net_range):
+                            if ipaddress.IPv4Address(b) in ipaddress.IPv4Network(
+                                net_range
+                            ):
                                 parsed_addresses.append(b)
                     except ValueError:
                         pass
@@ -130,53 +142,47 @@ def existing_ips_in_range(dev, netifaces, net_range, ipaddress):
     return parsed_addresses
 
 
-if __name__ == '__main__':
-    def privileged_main():
-        import os
-        import socket
-        import netifaces
-        import logging
-        import ipaddress
-        from kubernetes import client, config
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
 
-        logging.basicConfig(level=logging.INFO)
+    if os.getenv("L3LB_IN_K8S"):
+        config.load_incluster_config()
+    else:
+        config.load_kube_config()
 
-        if os.getenv('L3LB_IN_K8S'):
-            config.load_incluster_config()
-        else:
-            config.load_kube_config()
+    current_node = socket.gethostname()
+    network = os.getenv("L3LB_NETWORK")
+    interface = os.getenv("L3LB_INTERFACE")
 
-        current_node = socket.gethostname()
-        network = os.getenv('L3LB_NETWORK')
-        interface = os.getenv('L3LB_INTERFACE')
+    logging.info("using network " + network)
+    logging.info("using interface " + interface)
 
-        logging.info('using network ' + network)
-        logging.info('using interface ' + interface)
+    while True:
+        my_valid_ips = []
+        sleep(random.randrange(1, 60))
+        pods = get_pods(client)
 
-        while True:
-            my_valid_ips = []
-            sleep(random.randrange(1, 60))
-            pods = get_pods(client)
+        for lb in get_loadbalancers(client):
+            if local_pod_match(pods, lb, current_node, logging):
+                for ip in lb.spec.external_i_ps:
+                    my_valid_ips.append(ip)
 
-            for lb in get_loadbalancers(client):
-                if local_pod_match(pods, lb, current_node, logging):
-                    for ip in lb.spec.external_i_ps:
-                        my_valid_ips.append(ip)
-
-            '''
+        """
             in order to assure absence of leftover ips, or ips which belong to
             other nodes due to topology changes or pod migration, we create
             the list my_valid_ips and check any found addresses in the provided
             range against it.  this mechanism allows us to catch deletions -
             without any persisted state outside of kubernetes or the currently
             assigned addresses themselves.
-            '''
-            for ip in my_valid_ips:
-                provision_address(interface, ip, '/32', logging, netifaces, os)
+            """
+        for ip in my_valid_ips:
+            provision_address(interface, ip, "/32", logging, netifaces, os)
 
-            invalid_ips = list(set(existing_ips_in_range(interface, netifaces, network, ipaddress)).difference(my_valid_ips))
+        invalid_ips = list(
+            set(
+                existing_ips_in_range(interface, netifaces, network, ipaddress)
+            ).difference(my_valid_ips)
+        )
 
-            for ip in invalid_ips:
-                enforce_no_address(interface, ip, '/32', logging, netifaces, os)
-
-    privileged_main()
+        for ip in invalid_ips:
+            enforce_no_address(interface, ip, "/32", logging, netifaces, os)
